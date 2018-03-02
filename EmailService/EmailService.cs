648300dbf29be24de50288030eb32f9e5c619a0b
Michal <michal.schmidt95@gmail.com>
2018-03-02 15:01:53 +0100

    Add project files.

---
+++ EmailService/EmailService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mail;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace EmailService
+{
+    public partial class EmailService : ServiceBase
+    {
+        public EmailService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.Interval = 60000; //minuta
+            timer.Enabled = true;
+            timer.AutoReset = true;
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    SmtpClient client = new SmtpClient();
+                    client.Port = 587;
+                    client.Host = "smtp.gmail.com";
+                    client.EnableSsl = true;
+                    client.Timeout = 10000;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = new System.Net.NetworkCredential("user@gmail.com", "password");
+
+                    MailMessage mm = new MailMessage("donotreply@domain.com", "sendtomyemail@domain.co.uk", "test", "test");
+                    mm.BodyEncoding = UTF8Encoding.UTF8;
+                    mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+                    client.Send(mm);
+
+
+                };
+                channel.BasicConsume(queue: "hello",
+                    autoAck: true,
+                    consumer: consumer);
+
+            }
+        }
+
+        protected override void OnStop()
+        {
+            Dispose();
+        }
+    }
+}
